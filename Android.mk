2e9852ee5d7b5e7c03c6426a8d6b9d7f23290940
CuriousNom <aryanbiswas7585@gmail.com>
2025-09-17 09:21:11 +0530

    pipa: Drop firmware support
    Change-Id: Iea8b7f8541df85149cfd5acc4b2699f82f4743f5

--- Android.mk
+++ Android.mk
@@ -3,25 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),pipa)
-
-$(call add-radio-file-sha1-checked,radio/abl.img,2a0009be92280315233fd22667a1e9c93642512b)
-$(call add-radio-file-sha1-checked,radio/aop.img,6e6fe47fb221fb88ba6d94f438e281981fc29ce1)
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,a3971c6eb7e848dee2e78294023e3a7d0d3d48a8)
-$(call add-radio-file-sha1-checked,radio/cmnlib.img,032e068d001519ac452c0fad38338147a6a0dc84)
-$(call add-radio-file-sha1-checked,radio/cmnlib64.img,6fbe959383a7d0da210ce6a556b9d8daed96a246)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,4a9d7d9c812e83bd00dec4cc533d6679f56d9eef)
-$(call add-radio-file-sha1-checked,radio/dsp.img,e78cc34567eba97000f91f8577be0ad9e14d5de9)
-$(call add-radio-file-sha1-checked,radio/featenabler.img,416203e8166298952c3eb1278bed878806d1e10a)
-$(call add-radio-file-sha1-checked,radio/hyp.img,a9e3c7bd9554ce75aa283c41a5580d2696d70c83)
-$(call add-radio-file-sha1-checked,radio/imagefv.img,fdff7237bd0794f3b3ff7d3a2e8d09b05d87bb27)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,94ccf535d7f0ac614bfa03231ec5720da8d40818)
-$(call add-radio-file-sha1-checked,radio/modem.img,b368e9a73ab5df0098a94747fa240a921892231d)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,386bc0598c9cc63d23e239bf978fe86ea8b1c7cd)
-$(call add-radio-file-sha1-checked,radio/tz.img,3f194487b152b71e9c32bd20d761611b8483b235)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,dd3700cea73e87ed000ac02db73e4498956b50f7)
-$(call add-radio-file-sha1-checked,radio/xbl.img,571f18e134e9e1e8e79140237e28fb491912beb9)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,0da06f2ddba451ebca300f3e98262bba6f267563)
-
-endif
