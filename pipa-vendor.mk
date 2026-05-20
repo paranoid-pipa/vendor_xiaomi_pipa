1a305f3c206ddaf01e4f741e44b7bea6383d7011
CuriousNom <aryanbiswas7585@gmail.com>
2026-05-20 10:57:20 +0530

    pipa: Import libstagefrightdolby
    Change-Id: I3e9006a42a359082f54a9c01810fec7f104e0b86

--- pipa-vendor.mk
+++ pipa-vendor.mk
@@ -377,6 +377,7 @@ PRODUCT_PACKAGES += \
     libssrec \
     libstagefright_soft_ac4dec \
     libstagefright_soft_ddpdec \
+    libstagefrightdolby \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
     libwfdconfigutils_proprietary \
